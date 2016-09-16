ef7126861f336b708d9e6bf9e0bf1a831f8c8dc5
xD3Lx <mike.kravchenko.ua@gmail.com>
2016-09-16 02:26:28 +0200

    Completed first assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+
+        $scope.showMessage = function () {
+
+            var number = parseItemList($scope.items).length;
+
+            if (number == 0) {
+                $scope.message = 'Please enter data first';
+            } else if (number <= 3) {
+                $scope.message = 'Enjoy!';
+            } else {
+                $scope.message = 'Too much!';
+            }
+        };
+
+        function parseItemList(items) {
+            if (isEmpty(items)) {
+                return [];
+            } else {
+                return items.split(',')
+            }
+        }
+
+        function isEmpty(str) {
+            return (!str || 0 === str.length);
+        }
+    }
+})();
